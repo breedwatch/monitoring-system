6a0b0ae3678f5cc6dc5bda26c699bab334368d34
anderswodenker <handeln@anderswodenker.de>
2021-02-03 17:27:08 +0100

    0.1

--- sensorlib/dht22.py
+++ sensorlib/dht22.py
@@ -1,5 +1,6 @@
-import Adafruit_DHT
+import adafruit_dht
 from helper.error_helper import ErrorHandler
+import board
 
 
 class DHT22:
@@ -10,11 +11,16 @@ class DHT22:
 
     def get_data(self):
         try:
-            sensor = Adafruit_DHT.DHT22
-            humidity, temperature = Adafruit_DHT.read_retry(sensor, self.pin)
-            self.data = {"temp": round(float(temperature), 2), "hum": round(float(humidity), 2)}
+            sensor = adafruit_dht.DHT22(board.D21, use_pulseio=False)
+            # humidity, temperature = adafruit_dht.DHT22.read(sensor, self.pin)
+            # self.data = {"temp": round(float(temperature), 2), "hum": round(float(humidity), 2)}
+            self.data = sensor.temperature
+
+            print(self.data)
 
             return self.data
 
         except Exception as e:
+            print(e)
             self.error.log.exception(e)
+            return False
