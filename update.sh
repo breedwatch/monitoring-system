7ecd76a3ed2d2eb75cd4f5f7bbded71e3f83df26
anderswodenker <handeln@anderswodenker.de>
2021-05-01 19:35:10 +0200

    0.1.6rev5

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-rm -R beemo
-git clone https://github.com/breedwatch/monitoring-system beemo
+rm -R /home/pi/beemo
+git clone https://github.com/breedwatch/monitoring-system /home/pi/beemo
 chmod -R 777 /home/pi/beemo
 sudo timedatectl set-timezone Etc/UTC
 sudo rm conf.ini
-wget https://raw.githubusercontent.com/breedwatch/monitoring-system/main/conf.ini
-sudo chmod 777 conf.ini
+wget https://raw.githubusercontent.com/breedwatch/monitoring-system/main/conf.ini /home/pi/beemo/conf.ini
+sudo chmod 777 /home/pi/conf.ini
 
-#cd beemo && git pull origin main <-- TODO for later updates!
+if grep -Fxq "# 0.1.6rev4" /home/pi/beemo/main.py
+then
+   echo "ok!";
+else
+  echo "nope!"
+fi
