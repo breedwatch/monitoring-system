30012643579b0eecd2389e57d4b559640f871384
anderswodenker <handeln@anderswodenker.de>
2021-05-01 16:37:48 +0200

    Add update.sh

--- update.sh
+++ update.sh
@@ -3,5 +3,6 @@
 sudo rm -R /home/pi/beemo
 git clone https://github.com/breedwatch/monitoring-system /home/pi/beemo
 sudo chmod -R 777 /home/pi/beemo
+sudo timedatectl set-timezone Etc/UTC
 
 #cd beemo && git pull origin main <-- TODO for later updates!
